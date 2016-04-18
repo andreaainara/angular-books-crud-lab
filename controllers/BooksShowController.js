d43246a8f17e7e2d9229cb9fc9f33be605299cef
andreaainara <andrea.ainara10@gmail.com>
2016-04-18 12:20:02 -0700

    buttons and functions and things, oh my

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -21,36 +21,33 @@ function BooksShowController($http, $routeParams, $location) {
         console.log('show book error: ', error);
     }
 
-    vm.updateBook = function(bookToUpdate) {
-        console.log('someone wants to update: ', booktToUpdate);
+    vm.editBook = function() {
         $http({
             method: 'PUT',
-            url: 'https://super-crud.herokuapp.com/books/' + bookToUpdate._id,
+            url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
             data: {
-                title: bookToUpdate.title,
-                author: bookToUpdate.author,
-                image: bookToUpdate.image,
-                releaseDate: booktToUpdate.releaseDate
+                title: vm.book.title,
+                author: vm.book.author,
+                image: vm.book.image,
+                releaseDate: vm.book.releaseDate
             }
-        }).then(onBookUpdateSuccess, onError);
+        }).then(onBookEditSuccess, onError);
 
-        function onBookUpdateSuccess(response) {
+        function onBookEditSuccess(response) {
             console.log('successfully updated book: ', bookId, response.data);
             vm.book = response.data;
             $location.path('/');
         }
     };
 
-    vm.deleteBook = function(bookToDelete) {
-        console.log('someone wants to delete: ', bookToDelete);
+    vm.deleteBook = function() {
+        console.log('someone wants to delete: ');
         $http({
             method: 'DELETE',
-            url: 'https://super-crud.herokuapp.com/books/' + bookToDelete._id,
-        }).then(onBookDeleteSuccess, onError);
+            url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
+        }).then(function(response) {
+          $location.path('/');
+        });
 
-        function onBookDeleteSuccess(response) {
-            console.log('book delete response data: ', response.data);
-            $location.path('/');
-        }
     };
 }
