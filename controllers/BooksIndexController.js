c60d2669396a2d0da773ba67980e0a0df39aafbf
andreaainara <andrea.ainara10@gmail.com>
2016-04-18 11:09:57 -0700

    beginning stages, adding routes, attach view

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -1,7 +1,23 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+    .controller('BooksIndexController', BooksIndexController);
 
-BooksIndexController.$inject=['$http'];
-function BooksIndexController( $http ) {
-  var vm = this;
+BooksIndexController.$inject = ['$http'];
+
+function BooksIndexController($http) {
+    var vm = this;
+
+
+    $http({
+        method: 'GET',
+        url: 'https://super-crud.herokuapp.com/books'
+    }).then(onBooksIndexSuccess, onError);
+
+    function onBooksIndexSuccess(response) {
+        console.log('rendering books response data', response.data);
+        vm.books = response.data.books;
+    }
+
+    function onError(error) {
+        console.log('sad panda: ', error);
+    }
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,56 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  var bookId = $routeParams.id;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/'+ bookId
+  }).then(onBookShowSuccess, onError);
+
+  function onBookShowSuccess(reponse){
+    console.log('success showing books!', response.data);
+    vm.book = response.data;
+  }
+  function onError(error){
+    console.log('show book error: ', error);
+  }
+
+  vm.updateBook = function(bookToUpdate) {
+    console.log('someone wants to update: ', booktToUpdate);
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + bookToUpdate._id,
+      data: {
+        title: bookToUpdate.title,
+        author: bookToUpdate.author,
+        image: bookToUpdate.image,
+        releaseDate: booktToUpdate.releaseDate
+      }
+    }).then(onBookUpdateSuccess, onError);
+
+    function onBookUpdateSuccess(response) {
+      console.log('successfully updated book: ', bookId, response.data);
+      vm.book = response.data;
+      $location.path('/');
+    }
+  };
+
+  vm.deleteBook = function (bookToDelete) {
+    console.log('someone wants to delete: ', bookToDelete);
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + bookToDelete._id,
+    }).then(onBookDeleteSuccess, onError);
+
+    function blah(){
+
+    }
+  };
+
+
+
+}
